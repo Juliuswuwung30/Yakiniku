b0df2dd817e4f0253194030cd09f2be14a801afc
JuliusWuwung <127324193+Juliuswuwung30@users.noreply.github.com>
2023-11-11 15:45:59 +0700

    update detail

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -2,14 +2,25 @@
 @extends('layouts.navbar')
 @section('layout_contect')
 <div class="container">
-    <h1>{{ $product->name }}</h1>
+    <div class="row">
+        <!-- Column for the image -->
+        <div class="col-md-6">
+            <img src="{{ asset('images/' . $product->image) }}" alt="{{ $product->name }}" class="bd-placeholder-img card-img-top" height="500">
+        </div>
+        <!-- Column for the text -->
+        <div class="col-md-6">
+            <h1>{{ $product->name }}</h1>
+            <div class="card-body">
+                <p>Description: {{ $product->description }}</p>
+                <p>Price: {{ $product->price }}</p>
+                <p>Type: {{ $product->type }}</p>
 
-    <div class="card mb-4 shadow-sm">
-        <img src="{{ asset('images/' . $product->image) }}" alt="{{ $product->name }}" class="bd-placeholder-img card-img-top img-fluid" width="100%" height="225">
-        <div class="card-body">
-            <p>Description: {{ $product->description }}</p>
-            <p>Price: {{ $product->price }}</p>
-            <p>Type: {{ $product->type }}</p>
+                <!-- Buy button -->
+                <button type="button" class="btn btn-primary">Buy</button>
+
+                <!-- Favorite button -->
+                <button type="submit" class="btn btn-danger btn-lg text-end"><span class="bi bi-heart-fill"></span></button>
+            </div>
         </div>
     </div>
 </div>
