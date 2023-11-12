7de56532fa1e4a01c97b237e9379ab6b2de52c99
JuliusWuwung <127324193+Juliuswuwung30@users.noreply.github.com>
2023-11-12 22:12:36 +0700

    update about

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.navbar')
 
 @section('layout_contect')
-    <div class="container">
-        <div class="row mt-5 p-5">
-            <!-- Image column -->
-            <div class="col-md-6">
-                <img src="{{ url('/images/logo1.png') }}" alt="Additional Image">
+    <div class="container mt-5">
+        <div class="row p-3">
+            <!-- Image column (on smaller screens) -->
+            <div class="col-12 d-md-none mb-3">
+                <img src="{{ url('/images/logo1.png') }}" alt="Additional Image" class="img-fluid">
             </div>
 
             <!-- Content column -->
@@ -14,18 +14,22 @@
                 <p class="lead">Sebuah bisnis daging slice frozen dari Bojonegoro, ada bermacam-macam daging seperti ayam, sapi, ikan, bebek, dan bermacam-macam snack.</p>
                 <p class="lead">Ada juga produk-produk homemade seperti sauce yakiniku yang menambah kenikmatan saat BBQ.</p>
 
+                <!-- Contact section -->
                 <h2 class="mb-4"><strong>Contact Us</strong></h2>
                 <p class="lead">For any further questions or more information please contact us on WhatsApp!</p>
-                <ul class="d-flex align-items-center list-unstyled">
-                    <li>
+                <ul class="list-unstyled">
+                    <li class="d-flex align-items-center">
                         <img src="{{ url('/images/logowa.png') }}" alt="Contact Image" style="width: 50px; height: 50px;">
-                    </li>
-                    <li class="ms-3 d-flex align-items-center">
-                        <a href="https://wa.me/6285261731111">
+                        <a href="https://wa.me/6285261731111" class="ms-3">
                             <p>WhatsApp (Fransiska)</p>
                         </a>
                     </li>
                 </ul>
+            </div>
+
+            <!-- Image column (on larger screens) -->
+            <div class="col-md-6 d-none d-md-block">
+                <img src="{{ url('/images/logo1.png') }}" alt="Additional Image" class="img-fluid">
             </div>
         </div>
     </div>
